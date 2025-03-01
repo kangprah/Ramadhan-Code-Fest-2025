cff7229ce2f8acb4ed78ca468957124524cf361f
kyyril <khairilrahman55@gmail.com>
2025-03-01 11:30:57 +0700

    gataumales

---
+++ sobattakwa/README.md
@@ -0,0 +1,22 @@
+# SobatTakwa
+
+Aplikasi Islami yang dibangun menggunakan **Next.js** dan **Shadcn UI**, menawarkan fitur seperti jadwal sholat, pembacaan Al-Quran, dan doa harian.
+
+## Fitur
+
+### 1. **Jadwal Sholat**
+   - Cari dan pilih kota untuk melihat jadwal sholat.
+   - Menyediakan waktu sholat yang akurat berdasarkan lokasi yang dipilih.
+
+### 2. **Baca Al-Quran**
+   - Cari ayat berdasarkan nama ayat.
+   - Dengarkan audio setiap ayat.
+
+### 3. **Doa Harian**
+   - daftar doa harian.
+   - Cari doa berdasarkan nama doa.
+
+## Teknologi yang Digunakan
+- **Typescript** 
+- **Next.js**: Framework untuk aplikasi React dengan render di server.
+- **Shadcn UI + TailwindCSS**: Untuk membangun antarmuka pengguna yang elegan.
---
+++ sobattakwa/src/components/ToggleTheme.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import * as React from "react";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle() {
+  const { theme, setTheme, resolvedTheme } = useTheme(); // Use resolvedTheme to ensure the theme is correctly detected
+  const [mounted, setMounted] = React.useState(false);
+
+  // Only render the icons when the component has mounted
+  React.useEffect(() => setMounted(true), []);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  if (!mounted) {
+    return null; // Prevent rendering until the theme has been resolved
+  }
+
+  return (
+    <Button
+      className="rounded-xl hover:bg-transparent"
+      variant={"secondary"}
+      size="default"
+      onClick={toggleTheme}
+    >
+      <SunIcon
+        className={`h-[1.2rem] w-[1.2rem] transition-all duration-1000 ${
+          resolvedTheme === "dark" ? "rotate-0 scale-0 " : "rotate-0 scale-100 "
+        }`}
+      />
+      <MoonIcon
+        className={`absolute h-[1.2rem] w-[1.2rem] transition-all duration-1000 ${
+          resolvedTheme === "light"
+            ? "rotate-90 scale-0"
+            : "rotate-0 scale-100 "
+        }`}
+      />
+    </Button>
+  );
+}

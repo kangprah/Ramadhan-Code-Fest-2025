97c63d7f8265e70a1394a7d6ac3ddbe90acbdcbb
miruchigawa <miruchigawa@outlook.jp>
2025-03-01 09:37:36 +0000

    komit

---
+++ DiskortBot/Program.cs
@@ -0,0 +1,49 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using DiskortBot.Services;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddSingleton(new DiscordSocketClient(
+    new DiscordSocketConfig
+    {
+        GatewayIntents = Discord.GatewayIntents.Guilds | Discord.GatewayIntents.GuildMembers,
+        LogGatewayIntentWarnings = true,
+        AlwaysDownloadUsers = true,
+        LogLevel = Discord.LogSeverity.Info
+    }
+));
+
+builder.Services.AddSingleton<InteractionService>(provider =>
+    {
+        var client = provider.GetRequiredService<DiscordSocketClient>();
+        return new InteractionService(client);
+    }
+);
+
+builder.Services.Configure<InteractionServiceConfig>(options =>
+    {
+        options.LogLevel = Discord.LogSeverity.Info;
+        options.DefaultRunMode = RunMode.Async;
+    }
+);
+
+builder.Services.AddHttpClient<StableDiffusionService>((provider, client) =>
+    {
+        var config = provider.GetRequiredService<IConfiguration>();
+        client.BaseAddress = new Uri(config["StableDiffusion:Uri"]!);
+        client.Timeout = TimeSpan.FromMinutes(5);
+    }
+);
+
+builder.Services.AddHostedService<AppService>();
+
+var host = builder.Build();
+
+await host.RunAsync();
---
+++ DiskortBot/README.md
@@ -0,0 +1,15 @@
+# Diskort Bot Menggunakan C Hastag
+
+## Setup Environment & Run
+Pastikan dotnet terinstall minimal versi 8.0 bos.
+
+Sebelum itu atur beberapa Set User Secrets seperti
+```bash
+m1@LumiPyon ~ % dotnet user-secrets set "App:Token" "Discord Token Kamu"
+m1@LumiPyon ~ % dotnet user-secrets set "StableDiffusion:Uri" "Backend Stable Diffusion Kamu (AUTOMATIC111 stable diffusion webui)"
+
+```
+Nanti tinggal run aja yah!!
+
+## Hasil Generate
+<img src="generated.png" alt="gambar anime"/>
